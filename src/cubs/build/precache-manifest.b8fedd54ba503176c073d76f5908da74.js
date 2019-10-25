1ebbe88e3bae116c334597158ddfc5cc5bbbd3be
Ray Rojas <enciso.leo@gmail.com>
2019-10-25 01:17:04 +0000

    build

---
+++ src/cubs/build/precache-manifest.b8fedd54ba503176c073d76f5908da74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3fdaa11b181a16077cc9b02eec3f2e9",
+    "url": "/reactjs/index.html"
+  },
+  {
+    "revision": "0d6c1bbc78a94dcbeb62",
+    "url": "/reactjs/static/css/main.1c076d65.chunk.css"
+  },
+  {
+    "revision": "3c145fb1eb3ca7aa6295",
+    "url": "/reactjs/static/js/2.83a46bd0.chunk.js"
+  },
+  {
+    "revision": "0d6c1bbc78a94dcbeb62",
+    "url": "/reactjs/static/js/main.6d1542ab.chunk.js"
+  },
+  {
+    "revision": "6f7c38fc7ec1476f331d",
+    "url": "/reactjs/static/js/runtime-main.a8f644e3.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/reactjs/static/media/logo.25bf045c.svg"
+  }
+]);
