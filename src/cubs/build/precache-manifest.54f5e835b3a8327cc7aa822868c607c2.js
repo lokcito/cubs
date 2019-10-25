2116b0aca50c108005157d3431527f8e9e538cda
Ray Rojas <enciso.leo@gmail.com>
2019-10-25 15:31:46 +0000

    adding

---
+++ src/cubs/build/precache-manifest.54f5e835b3a8327cc7aa822868c607c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91adccb47cbd953afd18b4967e69cf9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e253fffaf38a59b95c5",
+    "url": "/static/css/main.82019f7a.chunk.css"
+  },
+  {
+    "revision": "3c145fb1eb3ca7aa6295",
+    "url": "/static/js/2.83a46bd0.chunk.js"
+  },
+  {
+    "revision": "3e253fffaf38a59b95c5",
+    "url": "/static/js/main.18983972.chunk.js"
+  },
+  {
+    "revision": "1f3c89d7d8e9d2ccca39",
+    "url": "/static/js/runtime-main.e89362ac.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
--- src/cubs/build/service-worker.js
+++ src/cubs/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactjs/precache-manifest.b8fedd54ba503176c073d76f5908da74.js"
+  "/precache-manifest.54f5e835b3a8327cc7aa822868c607c2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactjs/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
