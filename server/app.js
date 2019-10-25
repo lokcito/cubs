44a5b094ab51392b3b6fc43868ba741248fda9d9
Ray Rojas <enciso.leo@gmail.com>
2019-10-25 15:54:52 +0000

    :D

--- server/app.js
+++ server/app.js
@@ -16,10 +16,10 @@ app.post('/api/world', (req, res) => {
 });
 if (true || process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, '../src/cubs/build')));
+  app.use(express.static(path.join(__dirname, 'build')));
 // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, '../src/cubs/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 }
 app.listen(port, () => console.log(`Listening on port ${port}`));
